b1c7c69f79316081fa7a92313abc6c34f60f3616
sva01023 <savkinvadim13@gmail.com>
2022-05-12 15:37:57 +0300

    Практическая 2

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Витек'; // укажите здесь ваше имя
+
+$categories = array('Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное');
+
+$goods = [
+    ['Name'=>'2014 Rossignol District Snowboard','Category'=>'Доски и лыжи','price'=>10999,'Url'=>'img/lot-1.jpg'],
+    ['Name'=>'DC Ply Mens 2016/2017 Snowboard','Category'=>'Доски и лыжи','price'=>159999,'Url'=>'img/lot-2.jpg'],
+    ['Name'=>'Крепления Union Contact Pro 2015 года размер L/XL','Category'=>'Крепления','price'=>8000,'Url'=>'img/lot-3.jpg'],
+    ['Name'=>'Ботинки для сноуборда DC Mutiny Charocal','Category'=>'Ботинки','price'=>10999,'Url'=>'img/lot-4.jpg'],
+    ['Name'=>'Куртка для сноуборда DC Mutiny Charocal','Category'=>'Одежда','price'=>7500,'Url'=>'img/lot-5.jpg'],
+    ['Name'=>'Маска Oakley Canopy','Category'=>'Разное','price'=>5400,'Url'=>'img/lot-6.jpg'],
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -56,9 +68,11 @@ $user_name = 'Витек'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
-            </li>
+            <?php foreach($categories as $category) { ?>
+                <li class="promo__item promo__item--boards">
+                    <a class="promo__link" href="pages/all-lots.html"><?php echo $category?></a>
+                </li>
+            <?php } ?>
         </ul>
     </section>
     <section class="lots">
@@ -67,17 +81,18 @@ $user_name = 'Витек'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php foreach ($goods as $g) { ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?php echo $g['Url']?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?php echo $g['Category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php echo $g['Name']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?php echo $g['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -85,6 +100,7 @@ $user_name = 'Витек'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php } ?>
         </ul>
     </section>
 </main>
@@ -94,9 +110,11 @@ $user_name = 'Витек'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
-            </li>
+            <?php foreach($categories as $category) { ?>
+                <li class="nav__item">
+                    <a href="pages/all-lots.html"><?php echo $category?></a>
+                </li>
+            <?php } ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
