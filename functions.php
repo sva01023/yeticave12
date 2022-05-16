6a7062a3b04b6411b756260ebf257b5f670eb307
sva01023 <105356042+sva01023@users.noreply.github.com>
2022-05-16 13:45:24 +0300

    Практическая 5

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+function price ($price) {
+    $price =ceil($price);
+    if ($price >= 1000){
+        $result = number_format($price,0,' ',' ');
+    }
+    else $result = $price;
+    return $result . ' ₽';
+}
+
+function data_end () {
+    $current = time();
+    $tomorrowMidnight = strtotime('tomorrow');
+    $unix = $tomorrowMidnight - $current;
+
+    $h = floor($unix / 3600);
+    $m = floor(($unix % 3600) / 60);
+
+    if ($h < 10) {
+        $h = sprintf("%02d", $h);
+    }
+
+    if ($m < 10) {
+        $m = sprintf("%02d", $m);
+    }
+
+    return "{$h}:{$m}";
+}
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+    if (!file_exists($name)) {
+        return $result;
+    }
+    ob_start();
+    extract($data);
+    require($name);
+    $result = ob_get_clean();
+    return $result;
+}
+
+
+
---
+++ templates/index.php
@@ -0,0 +1,40 @@
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <!--заполните этот список из массива категорий-->
+        <?php foreach($categories as $category) { ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html"><?php echo $category?></a>
+            </li>
+        <?php } ?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <!--заполните этот список из массива с товарами-->
+        <?php foreach ($goods as $g) { ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?php echo $g['Url']?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?php echo $g['Category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php echo $g['Name']?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?php echo price($g['price'])?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            <?php echo data_end() ?>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php } ?>
+    </ul>
+</section>
